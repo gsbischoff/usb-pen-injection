cdf2948de55f01e7e8ab2d90d9a4f8a3bc3e71e4
gsbischoff <gsbischoff@gmail.com>
2018-05-19 16:28:55 -0400

    Made test window

--- IO Test/Point.c
+++ IO Test/Point.c
@@ -2,10 +2,90 @@
 
 #define WIN32_LEAN_AND_MEAN
 
+void printerr(DWORD err);
+LRESULT CALLBACK WindowProc(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam);
+
+
 int
 main(int argc, char **argv)
 {
 	WNDCLASS WindowClass = {0};
-	
+
+	WindowClass.style = CS_OWNDC;
+	WindowClass.lpfnWndProc = WindowProc;
+	WindowClass.hInstance = GetModuleHandle(0);
+	WindowClass.lpszClassName = "WindowClass";
+
+	if(RegisterClass(&WindowClass))
+	{
+		HWND WindowHandle = 
+			CreateWindowEx(
+				WS_EX_TRANSPARENT,
+				WindowClass.lpszClassName,
+				"Dummy",
+				WS_OVERLAPPEDWINDOW,
+				CW_USEDEFAULT,
+				CW_USEDEFAULT,
+				CW_USEDEFAULT,
+				CW_USEDEFAULT,
+				HWND_MESSAGE,
+				0,
+				GetModuleHandle(0),
+				0);
+		if(WindowHandle)
+		{
+			printf("Success!\n");
+
+			Sleep(1000);
+		}
+		else
+		{
+			printf("Failed!\n");
+
+			printerr(GetLastError());
+		}
+	}
 	return(0);
+}
+
+LRESULT CALLBACK
+WindowProc(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam)
+{
+	LRESULT Result = 0;
+
+	switch(Message)
+	{
+		case WM_POINTERUPDATE:
+		{
+			//GetPointer
+			printf("Pointer input!\n");
+			Result = DefWindowProc(Window, Message, WParam, LParam);
+		} break;
+
+		default:
+		{
+			Result = DefWindowProc(Window, Message, WParam, LParam);
+		} break;
+	}
+
+	return Result;
+}
+
+void
+printerr(DWORD err)
+{
+	char *buf;
+
+	DWORD result = FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM
+								|FORMAT_MESSAGE_ALLOCATE_BUFFER
+								|FORMAT_MESSAGE_IGNORE_INSERTS,
+								NULL,
+								err,
+								MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+								(LPTSTR) &buf,
+								0,
+								NULL);
+	printf("Error: %s\n", buf);
+
+	LocalFree(buf);
 }

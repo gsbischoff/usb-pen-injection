8439ad99bb6049e130d51859ac0f669d28f84484
gsbischoff <gsbischoff@gmail.com>
2018-05-22 00:55:31 -0400

    Created main startup file from which choice of server/client will be made

---
+++ PenMain.c
@@ -0,0 +1,87 @@
+//#include "NetworkHeader.h"
+#include <windows.h>
+
+LRESULT CALLBACK
+WindowProc(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam)
+{
+	LRESULT Result = 0;
+
+	switch(Message)
+	{
+		case WM_POINTERUPDATE:
+		{
+			//GetPointer
+			printf("Pointer input!\n");
+			Result = DefWindowProc(Window, Message, WParam, LParam);
+		} break;
+
+		default:
+		{
+			Result = DefWindowProc(Window, Message, WParam, LParam);
+		} break;
+	}
+
+	return Result;
+}
+
+int
+main()
+{
+	WNDCLASS WindowClass = {0};
+
+	WindowClass.style = CS_OWNDC;
+	WindowClass.lpfnWndProc = WindowProc;
+	WindowClass.hInstance = GetModuleHandle(0);
+	WindowClass.lpszClassName = "WindowClass";
+
+	if(RegisterClass(&WindowClass))
+	{
+		HWND WindowHandle = 
+			CreateWindowEx(
+				WS_EX_TRANSPARENT,
+				WindowClass.lpszClassName,
+				"Dummy",
+				WS_OVERLAPPEDWINDOW,
+				CW_USEDEFAULT,
+				CW_USEDEFAULT,
+				CW_USEDEFAULT,
+				CW_USEDEFAULT,
+				0, //HWND_MESSAGE,
+				0,
+				GetModuleHandle(0),
+				0);
+		
+		if(WindowHandle)
+		{
+			printf("Success!\n");
+
+			BOOL bRes = RegisterPointerDeviceNotifications(WindowHandle, TRUE);
+
+			if(!bRes)
+				printerr(GetLastError());
+
+			MSG Message;
+			for(;;)
+			{
+				BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+				if(MessageResult > 0)
+				{
+					printf("Got stuff!\n");
+					TranslateMessage(&Message);
+					DispatchMessage(&Message);
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+		else
+		{
+			printf("Failed!\n");
+
+			printerr(GetLastError());
+		}
+	}
+	return(0);
+}

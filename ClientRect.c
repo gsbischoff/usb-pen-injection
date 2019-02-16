33a28613f2380ca8d33c59980140144aac618def
Greg Bischoff <gsbischoff@gmail.com>
2019-02-15 22:38:41 -0500

    Made function to check DPI and screen resoltion

--- ClientRect.c
+++ ClientRect.c
@@ -1,37 +1,42 @@
 #include <windows.h>
 #include <stdio.h>
+
+#include <shtypes.h>
+#include <shellscalingapi.h>
+
+typedef struct
+{
+    int horizontalResolution;
+    int verticalResolution;
+
+    int horizontalDPI;
+    int verticalDPI;
+} resolution_settings;
+
+resolution_settings
+GetDeviceResolutionSettings()
+{
+    resolution_settings Result;
+
+    // Get desktop dc
+    HDC desktopDC = GetDC(NULL);
+
+    SetProcessDpiAwareness(PROCESS_SYSTEM_DPI_AWARE);
+
+    // Get native resolution
+    Result.horizontalResolution = GetDeviceCaps(desktopDC, HORZRES);
+    Result.verticalResolution = GetDeviceCaps(desktopDC, VERTRES); // Accidentally called 'VERZRES' in example in doc
+
+    // Get native resolution
+    Result.horizontalDPI = GetDeviceCaps(desktopDC, LOGPIXELSX);
+    Result.verticalDPI = GetDeviceCaps(desktopDC, LOGPIXELSY);
+
+    return(Result);
+}
+
 int
 main()
 {
-    RECT Rect = {0};
-    if(!GetClientRect(WindowFromDC(GetDC(NULL)), &Rect))
-    {
-        printf("failed!");
-    }
-    else
-    {
-        printf("(%d,%d)\n", Rect.bottom, Rect.right);
-    }
-
-    {
-        int _dpiX;
-        int _dpiY;
-        HDC hdc = GetDC(NULL); 
-        if (hdc) 
-        { 
-            _dpiX = GetDeviceCaps(hdc, LOGPIXELSX); 
-            _dpiY = GetDeviceCaps(hdc, LOGPIXELSY); 
-            ReleaseDC(NULL, hdc); 
-
-            printf("Caps: (%d,%d)\n", _dpiX, _dpiY);
-
-            // Retrieve the horizontal and vertical resolution of the current display setting. 
-            int cxScreen = GetSystemMetrics(SM_CXSCREEN); 
-            int cyScreen = GetSystemMetrics(SM_CYSCREEN); 
-
-            printf("Met: (%d,%d)\n", cxScreen, cyScreen);
-        } 
-
-    }
+    resolution_settings Settings = GetDeviceResolutionSettings();
     return(0);
 }
